8e8d00951b928f2cc9a24a505d2fdc6540d27d28
Alapan000 <alapandas2810@gmail.com>
2025-03-12 19:44:33 +0530

    independentScript.js is updated with code ideas

--- SigmaWebDev/JavaScript/JS Basics/independentScript.js
+++ SigmaWebDev/JavaScript/JS Basics/independentScript.js
@@ -3,3 +3,13 @@ console.log("Code is running..")
 console.log("Hi..")
 
 //Run using----> node filename.js
+
+/*
+We can use js to create multiple codes like
+
+-- organizing contents in a folder
+-- creating new folders
+-- moving files from one folder to another
+-- downloading files from internet
+
+*/
